660c4553f1a25ed41fb687111f7c9eaeab455249
emico-git <emilcu05@gmail.com>
2022-11-16 17:01:55 +0100

    working on header

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,25 @@
 import React from 'react'
+import AOS from 'aos';
+import 'aos/dist/aos.css'
 import logo from '../bilder/logo.png'
 
 export default function header() {
+
+  AOS.init();
+
   return (
     <div className='header'>
-        <img src={logo} width='200px'></img>
-        <div className='Titel'>Emil's Bilder Gallerie</div>
+        <div className='logo'>
+          <img src={logo} width='200px'></img>
+          <div className='logo-name'>
+            <div className='slide-border'>
+              <div className=' slide-in-right1'>Emil</div>
+              <div className=' slide-in-right2'>Collins</div>
+            </div>
+          </div>
+        </div>
+        <div className='titel'>Bildergalerie</div>
+        <div className='about-me'>Über mich</div>
     </div>
   )
 }
