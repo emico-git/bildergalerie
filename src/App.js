b617c26b800dcf3d9688269b14bf0924f4f2db75
emico-git <emilcu05@gmail.com>
2022-11-23 17:03:22 +0100

    started gallery

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header  from './components/header';
+import Galerie  from './components/galerie';
+
 function App() {
   return (
     <div className="App">
       <Header/>
-
+      <Galerie/>
     </div>
   );
 }
